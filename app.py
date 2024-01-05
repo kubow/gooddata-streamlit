fc7ff1ee1158e1e7ddb22a9d726f7393c0f359f6
kubow <kube.kubow@gmail.com>
2024-01-05 20:08:05 +0100

    first chatbot version (obsolete)

--- app.py
+++ app.py
@@ -1,14 +1,54 @@
 
-import streamlit as st
 from component import mycomponent
+import openai
+import streamlit as st
+
 
 def main():
+    #connect openai key
+    openai.api_key = st.secrets["OPENAI_API_KEY"]
+    if "openai_model" not in st.session_state:
+        st.session_state["openai_model"] = "gpt-3.5-turbo"
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+    
     st.set_page_config(
         layout="wide", page_icon="favicon.ico", page_title="Streamlit-GoodData integration demo"
     )
     st.sidebar.success("Select a demo above.")
     value = mycomponent(my_input_value="hello there")
     st.write("Received", value)
+
+    for message in st.session_state.messages:
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
+
+    if prompt := st.chat_input("What is up?"):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        # Display user message in chat message container
+        with st.chat_message("user"):
+            st.markdown(prompt)
+        # Display assitant message in chat message container
+        with st.chat_message("assistant"):
+            message_placeholder = st.empty()
+            full_response = ""
+            # Simulate stream of response with milliseconds delay
+            for response in openai.ChatCompletion.create(
+                model=st.session_state["openai_model"],
+                messages=[
+                    {"role": m["role"], "content": m["content"]}
+                    for m in st.session_state.messages
+                ],
+                #will provide lively writing
+                stream=True,
+            ):
+                #get content in response
+                full_response += response.choices[0].delta.get("content", "")
+                # Add a blinking cursor to simulate typing
+                message_placeholder.markdown(full_response + "▌")
+            message_placeholder.markdown(full_response)
+        # Add assistant response to chat history
+        st.session_state.messages.append({"role": "assistant", "content": full_response})
 
 if __name__ == "__main__":
     main()
